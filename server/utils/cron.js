c2f9896c139be2f639cc429041e2fc018f819c60
zubairwani <zubairwani49@gmail.com>
2025-07-09 10:50:26 +0530

    'updated'

--- server/utils/cron.js
+++ server/utils/cron.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 function initializeCronJobs(port) {
     // For testing, use '* * * * *' (every minute).
     // For production, use '*/14 * * * *' (every 14 minutes).
-    const cronSchedule = process.env.NODE_ENV === 'production' ? '*/14 * * * *' : '* * * * *';
+    const cronSchedule = process.env.NODE_ENV === 'production' ? '* * * * *' : '* * * * *';
 
     cron.schedule(cronSchedule, async () => {
         const jobStartTime = new Date().toISOString();
